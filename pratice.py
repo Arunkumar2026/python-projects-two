841819c83c428e3b93177efe8a2d9aab99eacdb2
Arun Kumar <akulakumar453@gmail.com>
2026-02-13 18:12:58 +0530

    This is pratice file

--- pratice.py
+++ pratice.py
@@ -36,7 +36,7 @@ while True:
     num = input("Enter you Number: ")
     printWord(num)
 '''
-
+'''
 def divisible(divident, divisor):
     if divident % divisor == 0:
         return True
@@ -48,3 +48,71 @@ while True:
     divisor = int(input("Enter the divisor number: "))
 
     print(divisible(divident, divisor))
+'''
+
+num = 2
+print(type(num))
+string = str(num)
+print(type(string))
+
+floating = float(num)
+print(type(floating))
+boolean = bool(num)
+print(type(boolean))
+print(num)
+a,b = 10,20
+print(a)
+
+c = d = e = 100
+print(c,d,e)
+
+s = 'arun'
+print(s)
+s1 = "kum"      \
+"ar"
+print(s1)
+s2 = '''aru
+n
+kumar'''
+print(s2)
+
+
+emptylist = []
+print(type(emptylist))
+emptytuple = ()
+print(type(emptytuple))
+emptydict = {}
+print(type(emptydict))
+emptyset = set()
+print(type(emptyset))
+
+
+
+list1 = [1,2,3,4,5,6,7,8,9]
+print(list1)
+list1.pop()
+print(list1)
+list1.append(9)
+print(list1)
+listslicing = list1[:]
+print(listslicing)
+listslicing2 = list1[1:]
+print(listslicing2)
+listslicing3 = list1[:2]
+print(listslicing3)
+n = "3"
+listslicing4 = list1[::int(n)]
+print(listslicing4)
+
+changelilst = list1[1] = 56 
+print(changelilst)
+print(list1)
+list1[3:7] = [0,0,0,0]
+print(list1)
+list1[2] = 20
+print(list1)
+list1.insert(2,10)
+print(list1)
+
+newlist = [1,'arun', True]
+print(newlist)
